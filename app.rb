c1f7682b4f459b364eb5ed364286733e3ba99e38
deniseb89 <denise_b89@hotmail.com>
2014-10-14 12:38:57 -0400

    Populates app with shell comments

--- app.rb
+++ app.rb
@@ -1,7 +1,83 @@
 require 'bundler'
 Bundler.require
 
+require_relative 'models/food'
+require_relative 'models/party'
+require_relative 'models/order'
+
 ActiveRecord::Base.establish_connection({
 	adapter: 'postgresql',
 	database: 'restaurant_db'
 	})
+
+# index: Display the navigation page for RESTaurant
+
+
+# index: Display all the available foods
+
+
+# new: Return a form for a new food
+
+
+# create: Creates a new food
+
+
+# edit: Returns a form to edit a food
+
+
+# update: Updates an existing food
+
+
+# show: Display a single food item and a list of all the parties that included it
+
+
+# destroy: Removes a food 
+
+
+
+
+
+# index: Display all the parties
+
+
+# new: Return a form for a new party
+
+
+# create: Creates a new party
+
+
+# edit: Returns a form to edit a party's details
+
+
+# update: Updates an existing party's details
+
+
+# show: Display a single party and options for adding a food item to the party
+
+
+# destroy: Removes a party
+
+
+
+
+
+# create: Creates a new order
+
+
+# edit: Change an item to no-charge
+
+
+# destroy: Removes an order
+
+
+
+
+
+# /parties/:id/receipt: Saves the party's receipt data to a file. Displays the content of the receipt. Offer the file for download.
+
+
+
+# /parties/:id/checkout: Marks the party as paid
+
+
+
